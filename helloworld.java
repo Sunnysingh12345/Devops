84e85f1d17c7c9892f23553a551ebaa5c9aae286
RATHAN04 <41317476+RATHAN04@users.noreply.github.com>
2021-06-13 19:23:55 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ class Simple
 {
   public static void main(String args[])
   {
-    System.out.println("Hello Hi This is First Java Prg");
+    System.out.println("Hello Hi This is First Java Prgs Looks Good");
   }
 }
   
