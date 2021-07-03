131e13b3efe5d3d71a757b4c448e304ae596ba62
RATHAN04 <41317476+RATHAN04@users.noreply.github.com>
2021-07-03 17:25:45 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,7 +3,7 @@ class Simple
   public static void main(String args[])
   {
     System.out.println("Hello Hi This is First Java Prgs Looks Good_nEw Changews For Email Notifications For Pipelines jobs");
-    System.out.println("*******************HI*****************************************************************************");
+    System.out.println("*******************HI This*****************************************************************************");
   }
 }
   
